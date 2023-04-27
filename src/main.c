33fbbd981e6602453f325bfc6d592490a2bb44f6
Andridge Munene <andridgemunene@MAC.local>
2023-04-27 11:21:07 +0300

    DAMN

--- src/main.c
+++ src/main.c
@@ -25,45 +25,6 @@ int main() {
     // Enable depth testing
     glEnable(GL_DEPTH_TEST);
 
-
-    /*
-    //
-    SerialPort port;
-    const char* port_name = "/dev/tty.usbserial-110";  // Change this to the correct port name
-    speed_t baudrate = 9600;
-    SerialPort_init(&port, port_name, baudrate);
-
-    // Check if the object was created successfully
-    if (SerialPort_get_fd(&port) == -1) {
-      return 1;
-    }
-    char buffer[BUFFER_SIZE];
-    int bytes_read;
-    float total_angle_x = 0.0f;
-    float total_angle_y = 0.0f;
-
-    while (1) {
-        // Read data from the serial port
-        bytes_read = SerialPort_read(&port, buffer, BUFFER_SIZE);
-
-        if (bytes_read > 0) {
-            // Print the data received
-         // printf("Data received: %s\n", buffer);
-           float total_angle_x, total_angle_y;
-             sscanf(buffer, "Total Angle X: %f Total Angle Y: %f", &total_angle_x, &total_angle_y);
-           // renderText(10, 10, buffer);
-            // Set the position for displaying the text
-          // glWindowPos2f(10, 10);
-
-            // Render the text
-       //   glutBitmapString(GLUT_BITMAP_HELVETICA_18, (const unsigned char*)buffer);
-        }
-
-        // Wait for 5 seconds
-        usleep(100000);
-    }
-
-    SerialPort_destroy(&port);*/
 SerialPort port;
 const char* port_name = "/dev/tty.usbserial-110";  // Change this to the correct port name
 speed_t baudrate = 9600;
@@ -79,19 +40,15 @@ int bytes_read;
 float total_angle_x ;
 float total_angle_y ;
 
-// Enable face culling
-
-
  // Loop until the user closes the window
   while (!glfwWindowShouldClose(window)) {
-    //
+    
      // Read data from the serial port
     bytes_read = SerialPort_read(&port, buffer, BUFFER_SIZE);
 
     if (bytes_read > 0) {
-        // Print the data received
       //  printf("Total Angle X & Y: %f\n",buffer);
-      sscanf(buffer, "%*s %*s %f %*s %*s %*s %f", &total_angle_x, &total_angle_y);
+        sscanf(buffer, "%*s %*s %f %*s %*s %*s %f", &total_angle_x, &total_angle_y);
         printf("Total Angle X: %f\n",total_angle_x);
         printf("Total Angle Y: %f\n",total_angle_y);
     }
@@ -117,126 +74,111 @@ float total_angle_y ;
 
     glScalef(5.5f, 5.5f, 5.5f);
    //glRotatef(glfwGetTime() * 50.0f, 0.0f, 1.0f, 0.0f);
-    // Draw the grid lines
-    //float size = 20.0f;
-    //float step = 1.0f;
-        //
-// Set the object coordinates
-float object_x = 3.5f;
-float object_y = 3.5f;
-// Set the color of the square
-glColor3f(1.0f, 0.0f, 1.0f); // purple color (red and blue)
+    // Set the object coordinates
+    float object_x = 3.5f;
+    float object_y = 3.5f;
+    // Set the color of the square
+    glColor3f(1.0f, 0.0f, 1.0f); // purple color (red and blue)
+    //
+    glPushMatrix();
+    glTranslatef(object_x, 2.5f, object_y); // set the object's position and lift it by 0.5 units along the y-axis
+    glRotatef(total_angle_y, 0.0f, 1.0f, 0.0f); // rotate around the y-axis
+    glRotatef(total_angle_x, 1.0f, 0.0f, 0.0f); // rotate around the x-axis
 
-//float x = total_angle_x * 10000;
-//float y = total_angle_x * 10000;
-//
-glPushMatrix();
-glTranslatef(object_x, 2.5f, object_y); // set the object's position and lift it by 0.5 units along the y-axis
-glRotatef(total_angle_y, 0.0f, 1.0f, 0.0f); // rotate around the y-axis
-glRotatef(total_angle_x, 1.0f, 0.0f, 0.0f); // rotate around the x-axis
+    // Draw bottom square
+    glBegin(GL_LINE_LOOP);
+    glVertex3f(-0.5f, 0.0f, -0.5f); // bottom left corner
+    glVertex3f(0.5f, 0.0f, -0.5f); // bottom right corner
+    glVertex3f(0.5f, 0.0f, 0.5f); // top right corner
+    glVertex3f(-0.5f, 0.0f, 0.5f); // top left corner
+    glEnd();
 
-// Draw bottom square
-glBegin(GL_LINE_LOOP);
-glVertex3f(-0.5f, 0.0f, -0.5f); // bottom left corner
-glVertex3f(0.5f, 0.0f, -0.5f); // bottom right corner
-glVertex3f(0.5f, 0.0f, 0.5f); // top right corner
-glVertex3f(-0.5f, 0.0f, 0.5f); // top left corner
-glEnd();
+    // Draw top square
+    glBegin(GL_LINE_LOOP);
+    glVertex3f(-0.5f, 1.0f, -0.5f); // bottom left corner
+    glVertex3f(0.5f, 1.0f, -0.5f); // bottom right corner
+    glVertex3f(0.5f, 1.0f, 0.5f); // top right corner
+    glVertex3f(-0.5f, 1.0f, 0.5f); // top left corner
+    glEnd();
 
-// Draw top square
-glBegin(GL_LINE_LOOP);
-glVertex3f(-0.5f, 1.0f, -0.5f); // bottom left corner
-glVertex3f(0.5f, 1.0f, -0.5f); // bottom right corner
-glVertex3f(0.5f, 1.0f, 0.5f); // top right corner
-glVertex3f(-0.5f, 1.0f, 0.5f); // top left corner
-glEnd();
+    // Draw connecting lines
+    glBegin(GL_LINES);
+    glVertex3f(-0.5f, 0.0f, -0.5f); // bottom left corner
+    glVertex3f(-0.5f, 1.0f, -0.5f); // top left corner
 
-// Draw connecting lines
-glBegin(GL_LINES);
-glVertex3f(-0.5f, 0.0f, -0.5f); // bottom left corner
-glVertex3f(-0.5f, 1.0f, -0.5f); // top left corner
+    glVertex3f(0.5f, 0.0f, -0.5f); // bottom right corner
+    glVertex3f(0.5f, 1.0f, -0.5f); // top right corner
 
-glVertex3f(0.5f, 0.0f, -0.5f); // bottom right corner
-glVertex3f(0.5f, 1.0f, -0.5f); // top right corner
+    glVertex3f(0.5f, 0.0f, 0.5f); // top right corner
+    glVertex3f(0.5f, 1.0f, 0.5f); // bottom right corner
 
-glVertex3f(0.5f, 0.0f, 0.5f); // top right corner
-glVertex3f(0.5f, 1.0f, 0.5f); // bottom right corner
+    glVertex3f(-0.5f, 0.0f, 0.5f); // top left corner
+    glVertex3f(-0.5f, 1.0f, 0.5f); // bottom left corner
+    glEnd();
 
-glVertex3f(-0.5f, 0.0f, 0.5f); // top left corner
-glVertex3f(-0.5f, 1.0f, 0.5f); // bottom left corner
-glEnd();
-
-glPopMatrix();
+    glPopMatrix();
 
 
-// Draw your object here
-// Draw the X axis
-glColor3f(1.0f, 0.0f, 0.0f); // red color
-glBegin(GL_LINES);
-glVertex3f(0.0f, 0.0f, 0.0f); // starting point
-glVertex3f(5.0f, 0.0f, 0.0f); // ending point
-glEnd();
+    // Draw your object here
+    // Draw the X axis
+    glColor3f(1.0f, 0.0f, 0.0f); // red color
+    glBegin(GL_LINES);
+    glVertex3f(0.0f, 0.0f, 0.0f); // starting point
+    glVertex3f(5.0f, 0.0f, 0.0f); // ending point
+    glEnd();
 
-// Draw the Y axis
-glColor3f(0.0f, 1.0f, 0.0f); // green color
-glBegin(GL_LINES);
-glVertex3f(0.0f, 0.0f, 0.0f); // starting point
-glVertex3f(0.0f, 5.0f, 0.0f); // ending point
-glEnd();
+    // Draw the Y axis
+    glColor3f(0.0f, 1.0f, 0.0f); // green color
+    glBegin(GL_LINES);
+    glVertex3f(0.0f, 0.0f, 0.0f); // starting point
+    glVertex3f(0.0f, 5.0f, 0.0f); // ending point
+    glEnd();
 
-// Draw the Z axis
-glColor3f(0.0f, 0.0f, 1.0f); // blue color
-glBegin(GL_LINES);
-glVertex3f(0.0f, 0.0f, 0.0f); // starting point
-glVertex3f(0.0f, 0.0f, 5.0f); // ending point
-glEnd();
-// Draw the grid lines on the XY plane
-glColor3f(0.5f, 0.5f, 0.5f); // set the color to gray
-glBegin(GL_LINES); // start drawing lines
-for(float i=1.0f; i<=5.0f; i+=1.0f) { // loop through the x and y values
-    glVertex3f(i, 0.0f, 0.0f); // set the starting point of a line on the x-axis
-    glVertex3f(i, 5.0f, 0.0f); // set the ending point of a line on the x-axis
-    glVertex3f(0.0f, i, 0.0f); // set the starting point of a line on the y-axis
-    glVertex3f(5.0f, i, 0.0f); // set the ending point of a line on the y-axis
-}
-glEnd(); // end drawing lines
-
-
-// Draw the grid lines on the XZ plane
-glBegin(GL_LINES); // start drawing lines
-for(float i=1.0f; i<=5.0f; i+=1.0f) { // loop through the x and z values
-    glVertex3f(i, 0.0f, 0.0f); // set the starting point of a line on the x-axis
-    glVertex3f(i, 0.0f, 5.0f); // set the ending point of a line on the x-axis
-    glVertex3f(0.0f, 0.0f, i); // set the starting point of a line on the z-axis
-    glVertex3f(5.0f, 0.0f, i); // set the ending point of a line on the z-axis
-}
-glEnd(); // end drawing lines
-
-// Draw the grid lines on the YZ plane
-glBegin(GL_LINES); // start drawing lines
-for(float i=1.0f; i<=5.0f; i+=1.0f) { // loop through the y and z values
-    glVertex3f(0.0f, i, 0.0f); // set the starting point of a line on the y-axis
-    glVertex3f(0.0f, i, 5.0f); // set the ending point of a line on the y-axis
-    glVertex3f(0.0f, 0.0f, i); // set the starting point of a line on the z-axis
-    glVertex3f(0.0f, 5.0f, i); // set the ending point of a line on the z-axis
-}
-glEnd(); // end drawing lines
-
-//
+    // Draw the Z axis
+    glColor3f(0.0f, 0.0f, 1.0f); // blue color
+    glBegin(GL_LINES);
+    glVertex3f(0.0f, 0.0f, 0.0f); // starting point
+    glVertex3f(0.0f, 0.0f, 5.0f); // ending point
+    glEnd();
+    // Draw the grid lines on the XY plane
+    glColor3f(0.5f, 0.5f, 0.5f); // set the color to gray
+    glBegin(GL_LINES); // start drawing lines
+    for(float i=1.0f; i<=5.0f; i+=1.0f) { // loop through the x and y values
+        glVertex3f(i, 0.0f, 0.0f); // set the starting point of a line on the x-axis
+        glVertex3f(i, 5.0f, 0.0f); // set the ending point of a line on the x-axis
+        glVertex3f(0.0f, i, 0.0f); // set the starting point of a line on the y-axis
+        glVertex3f(5.0f, i, 0.0f); // set the ending point of a line on the y-axis
+    }
+    glEnd(); // end drawing lines
 
 
-    
+    // Draw the grid lines on the XZ plane
+    glBegin(GL_LINES); // start drawing lines
+    for(float i=1.0f; i<=5.0f; i+=1.0f) { // loop through the x and z values
+        glVertex3f(i, 0.0f, 0.0f); // set the starting point of a line on the x-axis
+        glVertex3f(i, 0.0f, 5.0f); // set the ending point of a line on the x-axis
+        glVertex3f(0.0f, 0.0f, i); // set the starting point of a line on the z-axis
+        glVertex3f(5.0f, 0.0f, i); // set the ending point of a line on the z-axis
+    }
+    glEnd(); // end drawing lines
+
+    // Draw the grid lines on the YZ plane
+    glBegin(GL_LINES); // start drawing lines
+    for(float i=1.0f; i<=5.0f; i+=1.0f) { // loop through the y and z values
+        glVertex3f(0.0f, i, 0.0f); // set the starting point of a line on the y-axis
+        glVertex3f(0.0f, i, 5.0f); // set the ending point of a line on the y-axis
+        glVertex3f(0.0f, 0.0f, i); // set the starting point of a line on the z-axis
+        glVertex3f(0.0f, 5.0f, i); // set the ending point of a line on the z-axis
+    }
+    glEnd(); // end drawing lines
+
+    //
     // Swap buffers
     glfwSwapBuffers(window);
 
     // Poll for events
     glfwPollEvents();
 }
-
-
-
-
-
     // Clean up
     glfwTerminate();
     return 0;
